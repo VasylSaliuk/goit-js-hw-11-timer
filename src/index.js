8f8cda68d11fa869e4cdf3f8704e868c400c7869
Vasyl Saliuk <67233665+VasylSaliuk@users.noreply.github.com>
2021-01-08 13:01:56 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 import css from "./css/styles.css";
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.targetDate = targetDate;
+    this.timer = document.querySelector(selector);
+    this.days = this.timer.querySelector('[data-value="days"]');
+    this.hours = this.timer.querySelector('[data-value="hours"]');
+    this.mins = this.timer.querySelector('[data-value="mins"]');
+    this.secs = this.timer.querySelector('[data-value="secs"]');
+  }
+
+  count() {
+    this.counterTime();
+    setInterval(() => {
+      this.counterTime();
+    }, 1000);
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  updateClock({ days, hours, mins, secs }) {
+    this.days.textContent = days;
+    this.hours.textContent = hours;
+    this.mins.textContent = mins;
+    this.secs.textContent = secs;
+  }
+
+  counterTime() {
+    let time = this.targetDate - Date.now();
+    const times = this.getTimeComponents(time);
+    this.updateClock(times);
+  }
+}
+
+const counterUntilBirthday = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("May 03, 2021"),
+});
+
+counterUntilBirthday.count();
